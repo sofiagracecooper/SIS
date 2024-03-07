65ebe6e50aeb50d50d1d9fa573a4d156544c99c1
sofia.cooper <sofia.cooper@MU-C1-02.mullen.pvt.k12.co.us>
2024-03-07 09:17:19 -0700

    Almost finished pojo, going to calc GPA.

--- SIS/src/Setup.java
+++ SIS/src/Setup.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
 
 public class Setup {
+	//new ArrayList<Student> studentList=new ArrayList<Student> studentList();
 private String firstName;
 private String lastName;
 private double gpa;
 private ArrayList<Course> courses;
+private int period;
+private String className;
+private String letterGrade;
+
 
 public Student(String firstName, String lastName, double gpa, ArrayList<Course> courses) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.courses = courses;
 }
+public Course(int period, String className, String letterGrade)
+{
+this.period=period;
+this.className=className;
+this.letterGrade=letterGrade;
+}
+
 public ArrayList<Course> getCourses() {
+	//I think this needs to be changed to return the right objects. 
     return courses;
 }
 public String firstName getfirstName()
@@ -25,5 +38,10 @@ public String lastName getlastName()
 public double gpa getgpa()
 {
 	return gpa;
+}
+
+public double calculateGPA()
+{
+	
 }
 }
