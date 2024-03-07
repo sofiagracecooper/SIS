4f052e5eb56468d59f5a87d6ce7a8db8c0577ed2
sofia.cooper <sofia.cooper@MU-C1-02.mullen.pvt.k12.co.us>
2024-03-07 08:32:01 -0700

    Creating constructor for student objects.
    Doesn't run yet but is the start for methods to get student information.

--- SIS/src/Setup.java
+++ SIS/src/Setup.java
@@ -6,12 +6,24 @@ private String lastName;
 private double gpa;
 private ArrayList<Course> courses;
 
-public Student(String firstName, String lastName, ArrayList<Course> courses) {
+public Student(String firstName, String lastName, double gpa, ArrayList<Course> courses) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.courses = courses;
 }
 public ArrayList<Course> getCourses() {
     return courses;
+}
+public String firstName getfirstName()
+{
+	return firstName;
+}
+public String lastName getlastName()
+{
+	return lastName;
+}
+public double gpa getgpa()
+{
+	return gpa;
 }
 }
