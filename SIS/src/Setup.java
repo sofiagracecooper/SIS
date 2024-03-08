b3e3cabcf6b627a93e075ac1fc044c93db3ca9b1
Sofia <Sofia@10.0.0.158>
2024-03-07 23:30:04 -0700

    trying to finish the calculate gpa method.

--- SIS/src/Setup.java
+++ SIS/src/Setup.java
@@ -35,13 +35,44 @@ public String lastName getlastName()
 {
 	return lastName;
 }
-public double gpa getgpa()
-{
-	return gpa;
-}
 
-public double calculateGPA()
+private void calculateGPA()
 {
+	if (courses.isEmpty())
+	{
+		gpa=0.00;
+	
+	}
+	private double convertGradeToNum(char letterGrade) {
+	    if (grade == 'A') {
+	        return 4.0;
+	    } else if (letterGrade == 'A') {
+	        return 4.0;
+	    } else if (letterGrade == 'A-') {
+	        return 3.7;
+	    } else if (letterGrade == 'B+') {
+	        return 3.3;
+	    } else if (letterGrade == 'B') {
+	        return 3.0;
+	    } else if (letterGrade == 'B-') {
+	        return 2.7;
+	    } else if (letterGrade == 'C+') {
+	        return 2.3;
+	    } else if (letterGrade == 'C') {
+	        return 2.0;
+	    } else if (letterGrade == 'C-') {
+	        return 1.7;
+	    } else if (leterGrade == 'D+') {
+	        return 1.3;
+	    } else if (letterGrade == 'D') {
+	        return 1.0;
+	    } else if (letterGrade == 'F') {
+	        return 0.0;
+	    } else {
+	        return 0.0; 
+	    }
 	
 }
+
+	
 }
