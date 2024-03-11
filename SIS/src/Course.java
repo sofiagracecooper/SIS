04581d52c255769e22bc5f352030b10106ea07ed
luke.denezza <luke.denezza@MU-C1-15.mullen.pvt.k12.co.us>
2024-03-11 08:09:46 -0600

    Did the thing i did in my last commit

--- SIS/src/Course.java
+++ SIS/src/Course.java
@@ -1,4 +1,38 @@
 
-public class Course {
-
+public class Course 
+{
+	private int period;
+	private String className;
+	private String letterGrade;
+	
+	public int getPeriod()
+		{
+			return period;
+		}
+	public void setPeriod(int period)
+		{
+			this.period = period;
+		}
+	public String getClassName()
+		{
+			return className;
+		}
+	public void setClassName(String className)
+		{
+			this.className = className;
+		}
+	public String getLetterGrade()
+		{
+			return letterGrade;
+		}
+	public void setLetterGrade(String letterGrade)
+		{
+			this.letterGrade = letterGrade;
+		}
+	public Course(int period, String className, String letterGrade)
+		{
+		this.period=period;
+		this.className=className;
+		this.letterGrade=letterGrade;
+		}
 }
