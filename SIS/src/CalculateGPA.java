ac7a62f253bc1587055afaef1b0c26b6d56b6e9c
luke.denezza <luke.denezza@MU-C1-15.mullen.pvt.k12.co.us>
2024-03-11 08:13:50 -0600

    put grade if in

--- SIS/src/CalculateGPA.java
+++ SIS/src/CalculateGPA.java
@@ -1,4 +1,32 @@
 
 public class CalculateGPA {
+	private double convertGradeToNum(char letterGrade) {
+	    if (grade == 'A') {
+	        return 4.0;
+	    } else if (letterGrade == 'A') {
+	        return 4.0;
+	    } else if (letterGrade == 'A-') {
+	        return 3.7;
+	    } else if (letterGrade == 'B+') {
+	        return 3.3;
+	    } else if (letterGrade == 'B') {
+	        return 3.0;
+	    } else if (letterGrade == 'B-') {
+	        return 2.7;
+	    } else if (letterGrade == 'C+') {
+	        return 2.3;
+	    } else if (letterGrade == 'C') {
+	        return 2.0;
+	    } else if (letterGrade == 'C-') {
+	        return 1.7;
+	    } else if (leterGrade == 'D+') {
+	        return 1.3;
+	    } else if (letterGrade == 'D') {
+	        return 1.0;
+	    } else if (letterGrade == 'F') {
+	        return 0.0;
+	    } else {
+	        return 0.0; 
+	    }
 
 }
