b66112e479a7c5ab24b72976b8e9008729b00873
jeremiah.gerlach <jeremiah.gerlach@MU-C1-22.mullen.pvt.k12.co.us>
2024-03-11 08:39:24 -0600

    Start

--- SIS/src/CalculateGPA.java
+++ SIS/src/CalculateGPA.java
@@ -1,4 +1,7 @@
 public class CalculateGPA 
 	{
-
+	public static void main(String[] args)
+		{
+		
+		}
 	}
--- SIS/src/Sorting.java
+++ SIS/src/Sorting.java
@@ -1,4 +1,22 @@
 public class Sorting 
 	{
+	public static void main(String [] args)
+		{
+		System.out.println("How would you like to sort the students? \n1) Last Name \n2) GPA \n3) Period");
+		}
 	
+	public static void lastName()
+		{
+		
+		}
+	
+	public static void GPA()
+		{
+		
+		}
+	
+	public static void period()
+		{
+		
+		}
 	}
