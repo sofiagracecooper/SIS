57b01d8c911557eebaa48d2214953d0393798df7
jonathan.alexander <jonathan.alexander@MU-C1-11.mullen.pvt.k12.co.us>
2024-03-12 08:27:45 -0600

    Worked on add students methof

--- SIS/src/AddDeleteStudents.java
+++ SIS/src/AddDeleteStudents.java
@@ -4,15 +4,34 @@ import java.util.Scanner;
 public class AddDeleteStudents
 	{
 
-		public static void main(String[] args)
-			{
-				System.out.println("What is the name of your Army? (NOT the faction)");
-				firstName = userStringInput.nextLine();
- 
-			}
 		
 		public static void AddStudents()
 		{
+			Scanner userStringInput = new Scanner(System.in);
+			Scanner userIntInput = new Scanner(System.in);
+			Scanner userDoubleInput = new Scanner(System.in);
+			
+		
+			System.out.println("Input the first name of the student..");
+			String firstName =userStringInput .nextLine();
+			
+	
+			System.out.println("Input the last name of the student.");
+			String lastName = userStringInput.nextLine();
+			
+			
+			System.out.println("Input the GPA of the student");
+			double  GPA = userDoubleInput.nextDouble();
+			
+			System.out.println("Input the first class the student has.");
+			String classNameOne = userStringInput.nextLine();
+			
+			System.out.println("Input the last name of the student.");
+			String lastName = userStringInput.nextLine();
+			
+			
+			
+			
 			
 		}
 		
