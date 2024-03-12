8a65fdb6fa7f124931c93286ce0b60604e6b861f
luke.denezza <luke.denezza@MU-C1-15.mullen.pvt.k12.co.us>
2024-03-12 08:15:37 -0600

    Got the arrayList working! its called students

--- SIS/src/arrayLists.java
+++ SIS/src/arrayLists.java
@@ -1,26 +1,17 @@
 import java.io.File;
 import java.io.IOException;
-import java.io.FileNotFoundException;
+import //java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class arrayLists
 	{
-		ArrayList<Student> students = new ArrayList<Student>();
-		public static void fillArrayList() throws FileNotFoundException
+		static ArrayList<Student> students = new ArrayList<Student>();
+		public static void fillArrayList() throws IOException
 		{
 			Scanner myFile = new Scanner(new File("Studentlist.txt"));
 				while(myFile.hasNext())
 					{
-					students.add(new Student(myFile.next(), myFile.next(), myFile)
-					String firstName = myFile.next();
-					String lastName = myFile.next();
-					String className1 = myFile.next();
-					String classGrade1 = myFile.next();
-					String className2 = myFile.next();
-					String classGrade2 = myFile.next();
-					String className3 = myFile.next();
-					String classGrade3 = myFile.next();
-					
+					students.add(new Student(myFile.next(), myFile.next(), myFile.next(), myFile.next(), myFile.next(), myFile.next(), myFile.next(), myFile.next(), 0.0));
 
 					
 					}
