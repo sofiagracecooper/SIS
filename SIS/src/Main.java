0ef729e763e499166ab945a8fd306bc8a347a368
LukeDeNezza <149434106+LukeDeNezza@users.noreply.github.com>
2024-03-19 08:36:42 -0600

    started working on menu but I forgot to code on a new branch

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -4,8 +4,11 @@ public class Main
 	{
 		public static void main(String[] args) throws IOException
 		{
-		Database.fillArrayList();
-		Sorting.howToSort();
+		//Database.fillArrayList();
+		//Sorting.howToSort();
 		//CalculateGPA.calculateGPA();
+		
+		System.out.println("What would you like to do? \n(1) Add or delete a student \n(2) Change current grades/schedule \n(3) Sort students");
+		
 		}
 	}
