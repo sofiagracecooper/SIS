c173c8333be847dfd7d1e1abc812e61c7e4b59df
LukeDeNezza <149434106+LukeDeNezza@users.noreply.github.com>
2024-03-19 08:53:32 -0600

    Menu gettign work

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -9,9 +9,31 @@ public class Main
 		//Sorting.howToSort();
 		//CalculateGPA.calculateGPA();
 			
-			Scanner userStringInput = new Scanner(System.in);
+			Scanner userIntInput = new Scanner(System.in);
 		
 			System.out.println("What would you like to do? \n(1) Add or delete a student \n(2) Change current grades/schedule \n(3) Sort students");
-			
+			int answer = userIntInput.nextInt();
+			if(answer == 1)
+				{
+					System.out.println("What would you like to do? \n(1) Add a Student? \n (2) Delete a Student?");
+					int answer2 = userIntInput.nextInt();
+						if(answer2 == 1)
+							{
+								AddDeleteStudents.AddStudents();
+							}
+						else if(answer2 == 2)
+							{
+								AddDeleteStudents.DeleteStudent();
+							}
+
+				}
+			else if(answer == 2)
+				{
+					ChangeGradeAndSchedule();
+				}
+			else if(answer == 3)
+				{
+					
+				}
 		}
 	}
