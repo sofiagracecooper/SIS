9f3f9ce0881b45f6ca173492916177f36f07964d
LukeDeNezza <149434106+LukeDeNezza@users.noreply.github.com>
2024-03-19 09:24:18 -0600

    Got Change grade almost done class will shortly follow.

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -16,12 +16,12 @@ public class Main
 			if(answer == 1)
 				{
 					System.out.println("What would you like to do? \n(1) Add a Student? \n(2) Delete a Student?");
-					int answer2 = userIntInput.nextInt();
-						if(answer2 == 1)
+					answer = userIntInput.nextInt();
+						if(answer == 1)
 							{
 								AddDeleteStudents.AddStudents();
 							}
-						else if(answer2 == 2)
+						else if(answer == 2)
 							{
 								AddDeleteStudents.DeleteStudent();
 							}
@@ -29,7 +29,17 @@ public class Main
 				}
 			else if(answer == 2)
 				{
-					//ChangeGradeAndSchedule();
+					System.out.println("What would you like to do? \n(1) Change a student's Grade \n(2) Change a student's schedule");
+					int answerGradeAndClass = userIntInput.nextInt();
+						if(answerGradeAndClass == 1)
+							{
+								ChangeGradeAndClass.listStudents();
+								ChangeGradeAndClass.changeGrade();
+							}
+						else if(answer == 2)
+							{
+								//ChangeGradeAndClass.changeClass();
+							}
 				}
 			else if(answer == 3)
 				{
