8129880a37afcc4434b121a580227401731b5837
jeremiah.gerlach <jeremiah.gerlach@MU-C1-22.mullen.pvt.k12.co.us>
2024-03-20 15:06:23 -0600

    Finished sorting by all 3 periods

---
+++ SIS/src/SortByFirstPeriod.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+
+
+public class SortByFirstPeriod implements Comparator<Student>
+
+	{
+
+	public int compare(Student s1, Student s2)
+
+		{
+
+		return s1.getClassName1().compareTo(s2.getClassName1());
+
+		}
+
+	}
